e18dfe3cacf851f82fd4e1309f653fcb52d54e6f
Random people <98900217+justaguy6@users.noreply.github.com>
2023-04-03 10:02:46 -0300

    Update ScreenShotPlugin.hx

--- src/screenshotplugin/ScreenShotPlugin.hx
+++ src/screenshotplugin/ScreenShotPlugin.hx
@@ -8,6 +8,7 @@ import openfl.display.Bitmap;
 import openfl.display.BitmapData;
 import openfl.geom.Rectangle;
 import openfl.geom.Matrix;
+import open.utils.Assets;
 import flixel.input.keyboard.FlxKey;
 import flixel.input.FlxInput.FlxInputState;
 
@@ -24,7 +25,8 @@ class ScreenShotPlugin extends flixel.FlxBasic {
     private var shotDisplayBitmap:Bitmap;
     private var outlineBitmap:Bitmap;
     public static var enabled:Bool = true;
-    public static var screenshotKey:FlxKey = FlxKey.F2;
+    public static var screenshotKey:FlxKey = #if android || FlxG.android.justReleased.BACK #end;
+    public var FilePath:String = lime.system.System.applicationStorageDirectory;
     public static var saveFormat(default, set):FileFormatOption = PNG;
     public static function set_saveFormat(v:FileFormatOption) {
         if (v != JPEG && v != PNG) {
@@ -80,10 +82,12 @@ class ScreenShotPlugin extends flixel.FlxBasic {
         shot.bitmapData.draw(FlxG.stage, new Matrix(1, 0, 0, 1, -0, -0));
         var png:ByteArray = shot.bitmapData.encode(shot.bitmapData.rect, (saveFormat == PNG ? new openfl.display.PNGEncoderOptions() : new openfl.display.JPEGEncoderOptions()));
         png.position = 0;
-        var path = "screenshots/Screenshot " + Date.now().toString().split(":").join("-") + saveFormat;
-        if (!sys.FileSystem.exists("./screenshots/"))
-            sys.FileSystem.createDirectory("./screenshots/");
+        #if (sys || android)
+        var path = FilePath + "screenshots/Screenshot " + Date.now().toString().split(":").join("-") + saveFormat;
+        if (!Assets.exists("./screenshots/"))
+            sys.FileSystem.createDirectory(FilePath + "./screenshots/");
         sys.io.File.saveBytes(path, png);
+        #end
         flashSprite.alpha = 1;
         FlxTween.tween(flashSprite, {alpha: 0}, 0.25);
         shotDisplayBitmap.bitmapData = shot.bitmapData;
